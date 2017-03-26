6d7683b162d0254b0f65f80f3952dd7501dc195a
markdyousef <mark@clai.io>
2017-03-26 19:07:55 +0200

    added colors from style guide

---
+++ src/styles/colors.js
@@ -0,0 +1,18 @@
+const text = {
+    background: '#fff',
+    placeholder: 'rgba(19, 21, 23, 0.25)',
+    text: '#131517',
+    highlight: 'rgba(103, 193, 152, 0.5)'
+};
+
+const actions = {
+    background: '#fff',
+    text: 'rgba(19, 21, 23, 0.5)',
+    textActive: '#67C198',
+    commands: 'rgba(19, 21, 23, 0,35)'
+};
+
+const quote = {
+    placeholder: 'rgb(19, 21, 23, 0.25)',
+    text: '#797C80'
+};
