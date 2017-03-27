5bad3fdac025d8d75ed101cb04b73ac09e0e75fd
markdyousef <mark@clai.io>
2017-03-27 16:10:17 +0200

    created basic image upload

--- src/components/ImageButton/ImageButton.js
+++ src/components/ImageButton/ImageButton.js
@@ -2,18 +2,30 @@ import React, { Component, PropTypes } from 'react';
 import styled from 'styled-components';
 import { actionsColor } from '../../styles/colors';
 
-const Button = styled.span`
+const Button = styled.button`
     height: 25px;
     color: ${props => props.active ? actionsColor.textActive : actionsColor.text};
     cursor: pointer;
     font-size: 12px;
     display: flex;
     align-items: center;
+    background: #fff;
+    border: none;
+    outline: none;
 `;
+
+// const Upload = styled.input`
+//     display: none;
+// `;
 
 export default class ImageButton extends Component {
     static propTypes = {
-        title: PropTypes.string.isRequired
+        title: PropTypes.string.isRequired,
+        editorState: PropTypes.shape({
+            _immutable: PropTypes.object
+        }).isRequired,
+        setEditorState: PropTypes.func.isRequired,
+        close: PropTypes.func.isRequired
     }
     static defaultProps = {
     }
@@ -21,11 +33,31 @@ export default class ImageButton extends Component {
         super();
         this.state = {};
     }
+    onClick = () => {
+        this.input.value = null;
+        this.input.click();
+    }
+    onChange = (e) => {
+        const { setEditorState, editorState } = this.props;
+        const file = e.target.files[0];
+        // check file type
+        if (file.type.indexOf('image/') === 0) {
+            const src = URL.createObjectURL(file);
+            console.log(src);
+        }
+    }
     render() {
         const { title } = this.props;
         return (
-            <Button>
+            <Button onClick={this.onClick}>
                 {title}
+                <input
+                    type="file"
+                    accept="image/*"
+                    ref={(c) => { this.input = c; }}
+                    onChange={this.onChange}
+                    style={{ display: 'none' }}
+                />
             </Button>
         );
     }
