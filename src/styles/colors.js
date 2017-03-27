80b121c109ed861245e231a09de2946d24958806
markdyousef <mark@clai.io>
2017-03-27 12:37:45 +0200

    added colors and layouts

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,18 +1,18 @@
-const text = {
+export const textColor = {
     background: '#fff',
     placeholder: 'rgba(19, 21, 23, 0.25)',
     text: '#131517',
     highlight: 'rgba(103, 193, 152, 0.5)'
 };
 
-const actions = {
+export const actionsColor = {
     background: '#fff',
     text: 'rgba(19, 21, 23, 0.5)',
     textActive: '#67C198',
     commands: 'rgba(19, 21, 23, 0,35)'
 };
 
-const quote = {
+export const quoteColor = {
     placeholder: 'rgb(19, 21, 23, 0.25)',
     text: '#797C80'
 };
---
+++ src/styles/layouts.js
@@ -0,0 +1,7 @@
+export const boxLayout = {
+    boxShadow: '1px 1px 2px rgba(0, 0, 0, 0.05)',
+    borderRadius: '3px',
+    border: '#f2f2f2'
+};
+
+export const containerLayout = {};
