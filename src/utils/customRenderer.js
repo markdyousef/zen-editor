80dff4fa703f4f8dbac90e138ad409891e9e8b12
markdyousef <mark@clai.io>
2017-03-28 20:52:43 +0200

    added embed, separator and atomic components to custom renderer

--- src/utils/customRenderer.js
+++ src/utils/customRenderer.js
@@ -1,9 +1,13 @@
 // @flow
 import { Block } from './constants';
 import ImageBlock from '../components/Blocks/Image';
+import EmbedBlock from '../components/Blocks/Embed';
+import AtomicBlock from '../components/Blocks/Atomic';
+import SeparatorBlock from '../components/Blocks/Separator';
 
 export default (editorState:Object, setEditorState:Function) => (contentBlock:Object) => {
     const type = contentBlock.getType();
+    console.log(type);
     switch (type) {
     case Block.IMAGE:
         return {
@@ -11,6 +15,17 @@ export default (editorState:Object, setEditorState:Function) => (contentBlock:Ob
             props: {
                 editorState,
                 setEditorState
+            }
+        };
+    case Block.ATOMIC:
+        return {
+            component: AtomicBlock,
+            editable: false,
+            props: {
+                components: {
+                    embed: EmbedBlock,
+                    separator: SeparatorBlock
+                }
             }
         };
     default:
