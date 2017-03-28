6fcc78c9a7e2a0e49b8f3e109a173eccddb632b0
markdyousef <mark@clai.io>
2017-03-28 12:14:55 +0200

    changed styling from absolute to fixed

--- src/styles/layouts.js
+++ src/styles/layouts.js
@@ -1,6 +1,6 @@
 export function boxLayout() {
     return `
-        position: absolute;
+        position: fixed;
         background: #fff;
         boxShadow: 1px 1px 2px rgba(0, 0, 0, 0.05);
         borderRadius: 3px;
