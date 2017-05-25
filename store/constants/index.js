4eb5907dd3ae10864b526dbe50037e92d6d92f80
markdyousef <mark@clai.io>
2017-05-25 18:06:50 +0200

    Revert "Revert "Merge remote-tracking branch 'origin/master' into develop""
    This reverts commit 8a04a7e871cb6ee4380252daaa41a5892cc7b621.

---
+++ store/constants/index.js
@@ -0,0 +1,12 @@
+// @flow
+
+// STATE
+export const SET_EDITOR_STATE: string = 'cardEditor/SET_EDITOR_STATE';
+export const UPDATE_EDITOR_STATE: string = 'cardEditor/UPDATE_EDITOR_STATE';
+export const CLEAR_EDITOR_STATE: string = 'cardEditor/CLEAR_EDITOR_STATE';
+
+// BLOCKS
+export const INSERT_DATA_BLOCK: string = 'cardEditor/INSERT_DATA_BLOCK';
+export const UPDATE_DATA_BLOCK: string = 'cardEditor/UPDATE_DATA_BLOCK';
+export const DELETE_BLOCK: string = 'cardEditor/DELETE_BLOCK';
+export const CREATE_BLCOK: string = 'cardEditor/CREATE_BLCOK';
